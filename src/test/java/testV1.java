3bafdeb77e82a081c159c7d1895fc8769fc8a8af
Alissa-1 <alena.cheremukhina@gmail.com>
2022-08-18 23:18:23 +0300

    WebDriverManager setting added in dependencies and testV1

---
+++ src/test/java/testV1.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class testV1 {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+}
