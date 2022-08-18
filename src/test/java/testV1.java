a2c24488706e3ed1e97f1b92063dd37b3d83f2f3
Alissa-1 <alena.cheremukhina@gmail.com>
2022-08-18 23:33:12 +0300

    dependency updated with Selenide

--- src/test/java/testV1.java
+++ src/test/java/testV1.java
@@ -2,8 +2,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class testV1 {
@@ -17,10 +19,20 @@ public class testV1 {
     @BeforeEach
     void setupTest() {
         driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
     void teardown() {
         driver.quit();
+    }
+
+    @Test
+    void shouldTestPostOfForm() {
+        driver.get("http://localhost:9999");
     }
 }
