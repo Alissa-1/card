1a3d0d33cda245818ae258e504dcfa3c97d7b661
Alissa-1 <alena.cheremukhina@gmail.com>
2022-08-19 00:43:26 +0300

    testV1 passed: using subsequence method without data-test-id

--- src/test/java/testV1.java
+++ src/test/java/testV1.java
@@ -1,11 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 
 public class testV1 {
@@ -18,7 +19,6 @@ public class testV1 {
 
     @BeforeEach
     void setupTest() {
-        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -34,5 +34,23 @@ public class testV1 {
     @Test
     void shouldTestPostOfForm() {
         driver.get("http://localhost:9999");
+
+        // сделаем выборку из трех элементов, определив их по тегу input
+        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+
+        // отправим данные в текстовые поля формы
+        inputs.get(0).sendKeys("Василий");
+        inputs.get(1).sendKeys("+79998888888");
+
+        // отправим данные в чекбокс
+        driver.findElement(By.className("checkbox__box")).click();
+
+        // жмём на кнопку "Отправить"
+        driver.findElement(By.className("button")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText().trim();
+
+        Assertions.assertEquals(expected, actual);
     }
 }
